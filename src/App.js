8dbac61b271027b775333563e8850a99679bd558
nuno <ngplbc@gmail.com>
2020-02-06 15:37:48 +0000

    more booking

--- src/App.js
+++ src/App.js
@@ -12,7 +12,56 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            trips: [],
+            trips: [
+                {
+                    "id": 0,
+                    "driver": "Ed Watson",
+                    "start_trip": "2020-02-05T12:00:00.000Z",
+                    "end_trip": "2020-02-05T18:00:00.000Z",
+                    "destination": "Ribamar",
+                    "car_start_mileage": 40672,
+                    "car_end_mileage": 40677,
+                    "car_id": 1,
+                    "plate": "PG-08-70",
+                    "img_url": "https://res.cloudinary.com/drm2ot7ge/image/upload/v1580913034/Volkswagen/Captura_de_ecr%C3%A3_2020-02-05_%C3%A0s_10.23.25_AM_jvl8ub.png"
+                },
+                {
+                    "id": 1,
+                    "driver": "Ed Watson",
+                    "start_trip": "2020-02-06T12:00:00.000Z",
+                    "end_trip": "2020-02-06T18:00:00.000Z",
+                    "destination": "Alverca do Ribatejo",
+                    "car_start_mileage": 40672,
+                    "car_end_mileage": 40677,
+                    "car_id": 1,
+                    "plate": "PG-08-70",
+                    "img_url": "https://res.cloudinary.com/drm2ot7ge/image/upload/v1580913034/Volkswagen/Captura_de_ecr%C3%A3_2020-02-05_%C3%A0s_10.23.25_AM_jvl8ub.png"
+                },
+                {
+                    "id": 3,
+                    "destination": "Alameda",
+                    "start_trip": "2020-02-05T12:00:00.000Z",
+                    "end_trip": "2020-02-05T18:00:00.000Z",
+                    "car_start_mileage": 50672,
+                    "car_end_mileage": 50677,
+                    "car_id": 2,
+                    "plate": "PG-09-77",
+                    "img_url": "https://res.cloudinary.com/drm2ot7ge/image/upload/v1580913034/Volkswagen/Captura_de_ecr%C3%A3_2020-02-05_%C3%A0s_10.24.23_AM_nxoeww.png"
+                },
+                {
+                    "id": 4,
+                    "destination": "Vila Nova de Mil Fontes",
+                    "start_trip": "2020-03-05T12:00:00.000Z",
+                    "end_trip": "2020-03-05T15:00:00.000Z",
+                    "car_start_mileage": 50677,
+                    "car_end_mileage": 50682,
+                    "car_id": 2,
+                    "plate": "PG-09-77",
+                    "img_url": "https://res.cloudinary.com/drm2ot7ge/image/upload/v1580913034/Volkswagen/Captura_de_ecr%C3%A3_2020-02-05_%C3%A0s_10.24.23_AM_nxoeww.png"
+                },
+                
+            ],
+            cars: [],
             availableCars: [],
             filteredTripsByDriver: [],
             filterByDriver: "all",
@@ -21,15 +70,14 @@ class App extends React.Component {
     }
 
     async componentDidMount() {
-        const { trips } = await tripsHelper();
-        this.setState({ trips });
+        const { cars } = await tripsHelper();
+        this.setState({ cars });
         // sort trips arr by date
         // sort((a, b) => moment(b.startDate) - moment(a.startDate))
     }
 
     // Toggle modal visibility
     handleModalVisibility = (bool, trip) => {
-        console.log("HERE", trip);
         this.setState({
             isModalVisible: bool,
             selectedTrip: trip
@@ -55,6 +103,79 @@ class App extends React.Component {
         });
     };
 
+    handleFilterByDriver = driver => {
+        this.setState({ filterByDriver: driver });
+    };
+  
+    // Boonking trips
+    handleDateSubmit = (date, startTime, endTime) => {
+        // turn strings to moment objects
+        const selectedDay = moment(date);
+        const startTrip = moment(`${date} ${startTime}`);
+        const endTrip = moment(`${date} ${endTime}`);
+
+        // get all booked hours by car that are in the future
+        const now = moment().format();
+        const bookedDatesByCar = this.state.trips.reduce((acc, curr) => {
+            const newTrip = curr.start_trip > now ? { start_trip: curr.start_trip, end_trip: curr.end_trip } : null;
+            return {
+                ...acc,
+                [curr.car_id]: newTrip ? 
+                    [
+                        ...(acc[curr.car_id] ? acc[curr.car_id] : []),
+                        newTrip
+                    ] : [
+                        ...(acc[curr.car_id] ? acc[curr.car_id] : []),
+                    ]
+            };
+        }, {});
+        console.log("Booked trips by car: ", bookedDatesByCar);
+        
+        // check if selectd date and time overlaps any alerady booked trips
+        let unavailableCarIds = [];
+        for (const key in bookedDatesByCar) {
+            for (let i = 0; i < bookedDatesByCar[key].length; i++) {
+                const currentCarStartTrip = moment(
+                    bookedDatesByCar[key][i].start_trip
+                );
+                const currentCarEndTrip = moment(
+                    bookedDatesByCar[key][i].end_trip
+                );
+                // check if the selected day matches any already booked days
+                if (
+                    currentCarStartTrip.format("YYYY-MM-DD") ===
+                    selectedDay.format("YYYY-MM-DD")
+                ) {
+                    // check if there are overlapping hours
+                    if (
+                        (startTrip > currentCarStartTrip &&
+                            startTrip < currentCarEndTrip) ||
+                        (endTrip > currentCarStartTrip &&
+                            endTrip < currentCarEndTrip) ||
+                        (startTrip < currentCarStartTrip &&
+                            endTrip > currentCarEndTrip)    
+                    ) {
+                        // push car_id to unavailableCarIds arr if it's not already there 
+                        if (!unavailableCarIds.includes(Number(key))) unavailableCarIds.push(Number(key));
+                    } 
+                } 
+            }
+        }
+        console.log("Unavailable car ids: ", unavailableCarIds);
+
+        this.setState(
+            prevState => ({
+                    ...prevState,
+                    availableCars: prevState.cars.filter(car => !unavailableCarIds.includes(car.id))
+                }),
+                () => console.log("Available cars: ", this.state.availableCars)
+            );
+    };
+
+    cleanAvailableCars = () => {
+        this.setState({ availableCars: [] })
+    };
+
     handleSubmitBooking = () => {
         console.log("booked");
         this.props.history.push("/");
@@ -72,92 +193,22 @@ class App extends React.Component {
         // })
     };
 
-    handleFilterByDriver = driver => {
-        this.setState({ filterByDriver: driver });
-    };
-
-    // Boonking trips
-    handleDateSubmit = (date, startTime, endTime) => {
-        // turn strings to moment objects
-        const selectedDay = moment(date);
-        const startTrip = moment(`${date} ${startTime}`);
-        const endTrip = moment(`${date} ${endTime}`);
-        // console.log("DAY: ", moment("2020-02-04T12:00:00.000Z").format("YYYY-MM-DD"));
-        // get all booked hours by car
-        const bookedDatesByCar = this.state.trips.reduce((acc, curr) => {
-            return {
-                ...acc,
-                [curr.car_id]: [
-                    ...(acc[curr.car_id] ? acc[curr.car_id] : []),
-                    { start_trip: curr.start_trip, end_trip: curr.end_trip }
-                ]
-            };
-        }, {});
-
-        // check if selectd date and time overlaps any alerady booked trips
-        let carAvailability = {};
-        for (const key in bookedDatesByCar) {
-            for (let i = 0; i < bookedDatesByCar[key].length; i++) {
-                const currentCarStartTrip = moment(
-                    bookedDatesByCar[key][i].start_trip
-                );
-                const currentCarEndTrip = moment(
-                    bookedDatesByCar[key][i].end_trip
-                );
-                console.log(currentCarStartTrip.format("YYYY-MM-DD"));
-                // check if the selected day matches any already booked days
-                if (
-                    currentCarStartTrip.format("YYYY-MM-DD") ===
-                    selectedDay.format("YYYY-MM-DD")
-                ) {
-                    // check if there are overlapping hours
-                    if (
-                        (startTrip > currentCarStartTrip &&
-                            startTrip < currentCarEndTrip) ||
-                        (endTrip > currentCarStartTrip &&
-                            endTrip < currentCarEndTrip)
-                    ) {
-                        carAvailability[key] = false;
-                    } else {
-                        carAvailability[key] =
-                            carAvailability.hasOwnProperty(key) &&
-                            carAvailability[key] === false
-                                ? false
-                                : true;
-                    }
-                    // if selected day doesn't match any booked day for current car
-                } else {
-                    carAvailability[key] =
-                        carAvailability.hasOwnProperty(key) &&
-                        carAvailability[key] === false
-                            ? false
-                            : true;
-                }
-
-                this.setState(
-                    prevState => ({
-                        ...prevState,
-                        availableCars: Object.keys(carAvailability)
-                    }),
-                    () => console.log(this.state.availableCars)
-                );
-            }
-        }
-    };
-
     render() {
         const {
             trips,
+            availableCars,
             filterByDriver,
             isModalVisible,
             selectedTrip
         } = this.state;
 
+        // console.log(this.state.cars);
+
         return (
             <>
                 <nav>
                     <img
-                        class="vwds-logo"
+                        className="vwds-logo"
                         src="images/logo_vwds_header@2x.png"
                         alt="VW Digital Solutions logo"
                         width="225"
@@ -210,6 +261,8 @@ class App extends React.Component {
                                 render={() => (
                                     <BookTrip
                                         handleDateSubmit={this.handleDateSubmit}
+                                        availableCars={availableCars}
+                                        cleanAvailableCars={this.cleanAvailableCars}
                                     />
                                 )}
                             />

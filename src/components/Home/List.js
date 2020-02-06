6197af30bfab384326908a2691002bcac1981400
Edward Watson <e@edwardwatson.com|>
2020-02-06 22:32:55 +0000

    add pagination

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -3,22 +3,80 @@ import "./List.css";
 // import moment from "moment";
 import Trip from "./Trip";
 
+// EW: This works on logic found here https://codepen.io/nunosoares/pen/OeRvXv
+
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTrips: this.props.trips,
+      currentPage: 1,
+      tripsPerPage: 12
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
   render() {
-    const { trips, handleModalVisibility } = this.props;
+    // Logic for displaying current todos
+    const indexOfLastTrip = this.state.currentPage * this.state.tripsPerPage;
+
+    const indexOfFirstTrip = indexOfLastTrip - this.state.tripsPerPage;
+
+    const currentTrips = this.props.trips.slice(
+      indexOfFirstTrip,
+      indexOfLastTrip
+    );
+
+    const pageNumbers = [];
+    for (
+      let i = 1;
+      i <= Math.ceil(this.props.trips.length / this.state.tripsPerPage);
+      i++
+    ) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          className={number === this.state.currentPage ? "active" : ""}
+          onClick={this.handleClick}
+        >
+          {number}
+        </li>
+      );
+    });
+
     return (
       <>
         {/* <p>
           <em>Sorted by date</em>
         </p> */}
+        <div>
+          <p>
+            <em>{this.props.trips.length} reservations matching your query</em>
+          </p>
+        </div>
         <div className="listContainer">
-          {trips.map(trip => (
+          {currentTrips.map(trip => (
             <Trip
               key={trip.id}
               trip={trip}
-              handleModalVisibility={handleModalVisibility}
+              handleModalVisibility={this.props.handleModalVisibility}
             />
           ))}
+        </div>
+        <div className="pageNumbers">
+          <ul id="page-numbers">{renderPageNumbers}</ul>
         </div>
       </>
     );

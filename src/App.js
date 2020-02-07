6d18728a76226f7df156cebcbdcae289c7424558
Edward Watson <e@edwardwatson.com|>
2020-02-07 10:53:18 +0000

    addsuccesssignposting

--- src/App.js
+++ src/App.js
@@ -7,293 +7,294 @@ import Home from "./components/Home/Home";
 import BookTrip from "./components/BookTrip/BookTrip";
 import MapModal from "./components/Home/MapModal";
 import StartModal from "./components/Home/StartModal";
+import Success from "./components/SuccessSignposting/Success";
 import {
-    tripsHelperFn,
-    startTripHelperFn,
-    bookingHelperFn
+  tripsHelperFn,
+  startTripHelperFn,
+  bookingHelperFn
 } from "./helpers/tripsHelper";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            trips: [],
-            cars: [],
-            availableCars: [],
-            filterByDriver: "all",
-            sortByDate: "desc",
-            isMapModalVisible: false,
-            isStartModalVisible: false,
-            selectedTrip: {},
-            typeOfModal: ""
-        };
-    }
-
-    async componentDidMount() {
-        const { trips, cars } = await tripsHelperFn();
-        this.setState({ trips, cars });
-    }
-
-    componentDidUpdate() {
-        scroll.scrollToTop();
-    }
-
-    // Handle modals visibility
-    handleMapModalVisibility = (bool, trip) => {
-        this.setState({
-            isMapModalVisible: bool,
-            selectedTrip: trip
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: [],
+      trip: {},
+      cars: [],
+      availableCars: [],
+      filterByDriver: "all",
+      sortByDate: "desc",
+      isMapModalVisible: false,
+      isStartModalVisible: false,
+      selectedTrip: {},
+      typeOfModal: "",
+      latestBooking: ""
     };
+  }
 
-    handleEditModalVisibility = (bool, trip, type) => {
-        this.setState({
-            isStartModalVisible: bool,
-            selectedTrip: trip,
-            typeOfModal: type
-        });
-    };
+  async componentDidMount() {
+    const { trips, cars } = await tripsHelperFn();
+    this.setState({ trips, cars });
+    console.log(this.state.selectedTrip);
+  }
 
-    // Sorting and filtering
-    handleSortByDate = () => {
-        this.setState(prevState => {
-            return this.state.sortByDate === "desc"
-                ? {
-                      ...prevState.trips.sort(
-                          (a, b) => moment(a.start_trip) - moment(b.start_trip)
-                      ),
-                      sortByDate: "asc"
-                  }
-                : {
-                      ...prevState.trips.sort(
-                          (a, b) => moment(b.start_trip) - moment(a.start_trip)
-                      ),
-                      sortByDate: "desc"
-                  };
-        });
-    };
+  componentDidUpdate() {
+    scroll.scrollToTop();
+  }
 
-    handleFilterByDriver = driver => {
-        this.setState({ filterByDriver: driver });
-    };
+  // Handle modals visibility
+  handleMapModalVisibility = (bool, trip) => {
+    this.setState({
+      isMapModalVisible: bool,
+      selectedTrip: trip
+    });
+  };
 
-    // Boonking trips
-    handleDateSubmit = (date, startTime, endTime) => {
-        // turn strings to moment objects
-        const selectedDay = moment(date);
-        const startTrip = moment(`${date} ${startTime}`);
-        const endTrip = moment(`${date} ${endTime}`);
+  handleEditModalVisibility = (bool, trip, type) => {
+    this.setState({
+      isStartModalVisible: bool,
+      selectedTrip: trip,
+      typeOfModal: type
+    });
+  };
 
-        // get all booked hours by car that are in the future
-        const now = moment().format("YYYY-MM-DD HH:mm:ss.000Z");
-        // TODO: alienate to helper file
-        const bookedDatesByCar = this.state.trips.reduce((acc, curr) => {
-            const newTrip =
-                curr.end_trip > now
-                    ? { start_trip: curr.start_trip, end_trip: curr.end_trip }
-                    : null;
-            return {
-                ...acc,
-                [curr.car_id]: newTrip
-                    ? [...(acc[curr.car_id] ? acc[curr.car_id] : []), newTrip]
-                    : [...(acc[curr.car_id] ? acc[curr.car_id] : [])]
-            };
-        }, {});
-        console.log("Booked trips by car: ", bookedDatesByCar);
+  // Sorting and filtering
+  handleSortByDate = () => {
+    this.setState(prevState => {
+      return this.state.sortByDate === "desc"
+        ? {
+            ...prevState.trips.sort(
+              (a, b) => moment(a.start_trip) - moment(b.start_trip)
+            ),
+            sortByDate: "asc"
+          }
+        : {
+            ...prevState.trips.sort(
+              (a, b) => moment(b.start_trip) - moment(a.start_trip)
+            ),
+            sortByDate: "desc"
+          };
+    });
+  };
 
-        // check if selectd date and time overlaps any alerady booked trips
-        // TODO alienate to helper file
-        let unavailableCarIds = [];
-        for (const key in bookedDatesByCar) {
-            for (let i = 0; i < bookedDatesByCar[key].length; i++) {
-                const currentCarStartTrip = moment(
-                    bookedDatesByCar[key][i].start_trip
-                );
-                const currentCarEndTrip = moment(
-                    bookedDatesByCar[key][i].end_trip
-                );
-                // check if there are overlapping hours
-                if (
-                    (startTrip >= currentCarStartTrip &&
-                        startTrip <= currentCarEndTrip) ||
-                    (endTrip >= currentCarStartTrip &&
-                        endTrip <= currentCarEndTrip) ||
-                    (startTrip <= currentCarStartTrip &&
-                        endTrip >= currentCarEndTrip) 
-                ) {
-                    // push car_id to unavailableCarIds arr if it's not already there
-                    if (!unavailableCarIds.includes(Number(key)))
-                        unavailableCarIds.push(Number(key));
-                }
-                console.log("Unavailable car ids: ", unavailableCarIds);
+  handleFilterByDriver = driver => {
+    this.setState({ filterByDriver: driver });
+  };
 
-                this.setState(prevState => ({
-                    ...prevState,
-                    availableCars: prevState.cars.filter(car => !unavailableCarIds.includes(car.id))
-                }));
-            }
+  // Boonking trips
+  handleDateSubmit = (date, startTime, endTime) => {
+    // turn strings to moment objects
+    const selectedDay = moment(date);
+    const startTrip = moment(`${date} ${startTime}`);
+    const endTrip = moment(`${date} ${endTime}`);
+
+    // get all booked hours by car that are in the future
+    const now = moment().format("YYYY-MM-DD HH:mm:ss.000Z");
+    // TODO: alienate to helper file
+    const bookedDatesByCar = this.state.trips.reduce((acc, curr) => {
+      const newTrip =
+        curr.end_trip > now
+          ? { start_trip: curr.start_trip, end_trip: curr.end_trip }
+          : null;
+      return {
+        ...acc,
+        [curr.car_id]: newTrip
+          ? [...(acc[curr.car_id] ? acc[curr.car_id] : []), newTrip]
+          : [...(acc[curr.car_id] ? acc[curr.car_id] : [])]
+      };
+    }, {});
+    console.log("Booked trips by car: ", bookedDatesByCar);
+
+    // check if selectd date and time overlaps any alerady booked trips
+    // TODO alienate to helper file
+    let unavailableCarIds = [];
+    for (const key in bookedDatesByCar) {
+      for (let i = 0; i < bookedDatesByCar[key].length; i++) {
+        const currentCarStartTrip = moment(bookedDatesByCar[key][i].start_trip);
+        const currentCarEndTrip = moment(bookedDatesByCar[key][i].end_trip);
+        // check if there are overlapping hours
+        if (
+          (startTrip >= currentCarStartTrip &&
+            startTrip <= currentCarEndTrip) ||
+          (endTrip >= currentCarStartTrip && endTrip <= currentCarEndTrip) ||
+          (startTrip <= currentCarStartTrip && endTrip >= currentCarEndTrip)
+        ) {
+          // push car_id to unavailableCarIds arr if it's not already there
+          if (!unavailableCarIds.includes(Number(key)))
+            unavailableCarIds.push(Number(key));
         }
-    };
+        console.log("Unavailable car ids: ", unavailableCarIds);
 
-    handleOnBooking = trip => {
-        bookingHelperFn(trip);
         this.setState(prevState => ({
-            ...prevState,
-            trips: [trip, ...prevState.trips]
+          ...prevState,
+          availableCars: prevState.cars.filter(
+            car => !unavailableCarIds.includes(car.id)
+          )
         }));
-    };
-
-    cleanAvailableCars = () => {
-        this.setState({ availableCars: [] });
-    };
-
-    // Start and end trips
-    editTripHandler = (trip, mileage, type) => {
-        // trip with mileage
-        const updatedTrip =
-            type === "start"
-                ? {
-                      ...trip,
-                      start_trip: moment().format("YYYY-MM-DD HH:mm:ss.000Z"),
-                      car_start_mileage: mileage,
-                      car_end_mileage: mileage
-                  }
-                : {
-                      ...trip,
-                      end_trip: moment().format("YYYY-MM-DD HH:mm:ss.000Z"),
-                      car_end_mileage: mileage
-                  };
-        // update the DB
-        startTripHelperFn(updatedTrip);
-        // update the state with a new trips
-        this.setState(prevState => {
-            const updatedTrips = prevState.trips.map(trip => {
-                if (trip.id === updatedTrip.id) {
-                    return updatedTrip;
-                }
-                return trip;
-            });
-            return {
-                ...prevState,
-                trips: updatedTrips,
-                isStartModalVisible: false,
-                selectedTrip: {}
-            };
-        });
-    };
-
-    render() {
-        const {
-            trips,
-            availableCars,
-            filterByDriver,
-            isMapModalVisible,
-            isStartModalVisible,
-            typeOfModal,
-            selectedTrip
-        } = this.state;
-
-        return (
-            <>
-                <nav>
-                    <Link to="/">
-                        <img
-                            className="vwds-logo"
-                            src="images/logo_vwds_header@2x.png"
-                            alt="VW Digital Solutions logo"
-                            width="225"
-                            height="36"
-                        ></img>
-                    </Link>
-                    <Link to="/">
-                        <img
-                            className="vwds-logo-mobile"
-                            src="images/logo-mobile.png"
-                            alt="VW Digital Solutions logo"
-                            width="36"
-                            height="36"
-                        ></img>
-                    </Link>
-                </nav>
-
-                <div className="App">
-                    <header className="App-header">
-                        <Switch>
-                            {isStartModalVisible && (
-                                <StartModal
-                                    type={typeOfModal}
-                                    trip={selectedTrip}
-                                    isModalVisible={isStartModalVisible}
-                                    handleModalVisibility={
-                                        this.handleEditModalVisibility
-                                    }
-                                    onEditTripSubmit={this.editTripHandler}
-                                />
-                            )}
-
-                            {isMapModalVisible && (
-                                <MapModal
-                                    trip={selectedTrip}
-                                    isModalVisible={isMapModalVisible}
-                                    handleModalVisibility={
-                                        this.handleMapModalVisibility
-                                    }
-                                />
-                            )}
-
-                            <Route
-                                exact
-                                path="/"
-                                render={() => (
-                                    <Home
-                                        trips={trips}
-                                        tripsFilteredByDriver={
-                                            filterByDriver !== "all"
-                                                ? trips.filter(
-                                                      trip =>
-                                                          trip.driver ===
-                                                          filterByDriver
-                                                  )
-                                                : []
-                                        }
-                                        onSortByDate={this.handleSortByDate}
-                                        onFilterByDriver={
-                                            this.handleFilterByDriver
-                                        }
-                                        isMapModalVisible={isMapModalVisible}
-                                        handleMapModalVisibility={
-                                            this.handleMapModalVisibility
-                                        }
-                                        handleEditModalVisibility={
-                                            this.handleEditModalVisibility
-                                        }
-                                    />
-                                )}
-                            />
-                            <Route
-                                exact
-                                path="/booking"
-                                render={() => (
-                                    <BookTrip
-                                        handleDateSubmit={this.handleDateSubmit}
-                                        availableCars={availableCars}
-                                        cleanAvailableCars={
-                                            this.cleanAvailableCars
-                                        }
-                                        onBooking={this.handleOnBooking}
-                                    />
-                                )}
-                            />
-                        </Switch>
-                    </header>
-                    <footer>
-                        CREATED AT THE WILD CODE SCHOOL LISBON HACKATHON FOR
-                        VWDS, © 2020.
-                    </footer>
-                </div>
-            </>
-        );
+      }
     }
+  };
+
+  handleOnBooking = trip => {
+    bookingHelperFn(trip);
+
+    this.setState(prevState => ({
+      ...prevState,
+      trips: [trip, ...prevState.trips],
+      trip: trip
+    }));
+
+    //    trip is added to state. this will be used to show successcomponent
+  };
+
+  cleanAvailableCars = () => {
+    this.setState({ availableCars: [] });
+  };
+
+  // Start and end trips
+  editTripHandler = (trip, mileage, type) => {
+    // trip with mileage
+    const updatedTrip =
+      type === "start"
+        ? {
+            ...trip,
+            start_trip: moment().format("YYYY-MM-DD HH:mm:ss.000Z"),
+            car_start_mileage: mileage,
+            car_end_mileage: mileage
+          }
+        : {
+            ...trip,
+            end_trip: moment().format("YYYY-MM-DD HH:mm:ss.000Z"),
+            car_end_mileage: mileage
+          };
+    // update the DB
+    startTripHelperFn(updatedTrip);
+    // update the state with a new trips
+    this.setState(prevState => {
+      const updatedTrips = prevState.trips.map(trip => {
+        if (trip.id === updatedTrip.id) {
+          return updatedTrip;
+        }
+        return trip;
+      });
+      return {
+        ...prevState,
+        trips: updatedTrips,
+        isStartModalVisible: false,
+        selectedTrip: {}
+      };
+    });
+  };
+
+  render() {
+    const {
+      trips,
+      availableCars,
+      filterByDriver,
+      isMapModalVisible,
+      isStartModalVisible,
+      typeOfModal,
+      selectedTrip
+    } = this.state;
+
+    return (
+      <>
+        <nav>
+          <Link to="/">
+            <img
+              className="vwds-logo"
+              src="images/logo_vwds_header@2x.png"
+              alt="VW Digital Solutions logo"
+              width="225"
+              height="36"
+            ></img>
+          </Link>
+          <Link to="/">
+            <img
+              className="vwds-logo-mobile"
+              src="images/logo-mobile.png"
+              alt="VW Digital Solutions logo"
+              width="36"
+              height="36"
+            ></img>
+          </Link>
+        </nav>
+
+        <div className="App">
+          <header className="App-header">
+            <Switch>
+              {isStartModalVisible && (
+                <StartModal
+                  type={typeOfModal}
+                  trip={selectedTrip}
+                  isModalVisible={isStartModalVisible}
+                  handleModalVisibility={this.handleEditModalVisibility}
+                  onEditTripSubmit={this.editTripHandler}
+                />
+              )}
+
+              {isMapModalVisible && (
+                <MapModal
+                  trip={selectedTrip}
+                  isModalVisible={isMapModalVisible}
+                  handleModalVisibility={this.handleMapModalVisibility}
+                />
+              )}
+
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <Home
+                    trips={trips}
+                    tripsFilteredByDriver={
+                      filterByDriver !== "all"
+                        ? trips.filter(trip => trip.driver === filterByDriver)
+                        : []
+                    }
+                    onSortByDate={this.handleSortByDate}
+                    onFilterByDriver={this.handleFilterByDriver}
+                    isMapModalVisible={isMapModalVisible}
+                    handleMapModalVisibility={this.handleMapModalVisibility}
+                    handleEditModalVisibility={this.handleEditModalVisibility}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/booking"
+                render={() => (
+                  <BookTrip
+                    handleDateSubmit={this.handleDateSubmit}
+                    availableCars={availableCars}
+                    cleanAvailableCars={this.cleanAvailableCars}
+                    onBooking={this.handleOnBooking}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/bookingsuccess"
+                render={() => (
+                  <Success
+                    header={"You've booked a car!"}
+                    subheader={"We've got you covered."}
+                    paragraph={`Once you get in the car, find your trip in Current Trips, and just tap START TRIP. Enter the start mileage from the car's dial, then you’re ready to drive!`}
+                    trip={this.state.trip}
+                    buttontext={"OK"}
+                    buttonlinkdestination={"/"}
+                  />
+                )}
+              />
+            </Switch>
+          </header>
+          <footer>
+            CREATED AT THE WILD CODE SCHOOL LISBON HACKATHON FOR VWDS, © 2020.
+          </footer>
+        </div>
+      </>
+    );
+  }
 }
 
 export default withRouter(App);

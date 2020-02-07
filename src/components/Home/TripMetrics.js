8ef4767d5b93da4295e2842d9e8fb8b46a324e89
Edward Watson <e@edwardwatson.com|>
2020-02-07 13:42:19 +0000

    addrouteetctometrics

--- src/components/Home/TripMetrics.js
+++ src/components/Home/TripMetrics.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link, Router } from "react-router-dom";
 
 class TripMetrics extends React.Component {
-    render() {
-        
-        return (
-            <>
-                <p>metrics</p>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="button">
+          <Link to="/">HOME</Link>
+        </div>
+        <h1>Metrics</h1>
+        <h2>Filter by car or by person.</h2>
+        <br />
+      </>
+    );
+  }
 }
 export default TripMetrics;
